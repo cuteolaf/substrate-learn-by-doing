4556d3e0f9eeb3b743f88bb6c630b15ec75dee25
cuteolaf <oliverlim818@gmail.com>
2022-08-16 22:25:17 -0700

    add: mapping variable

--- smart-contract/incrementer/lib.rs
+++ smart-contract/incrementer/lib.rs
@@ -5,28 +5,40 @@ use ink_lang as ink;
 #[ink::contract]
 mod incrementer {
 
+    use ink_storage::traits::SpreadAllocate;
+
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
+    #[derive(SpreadAllocate)]
     pub struct Incrementer {
-        /// Stores a single `bool` value on the storage.
+        // Store a maaping from AccountIds to a u32
         value: i32,
+        my_value: ink_storage::Mapping<AccountId, i32>,
     }
 
     impl Incrementer {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
         pub fn new(init_value: i32) -> Self {
-            Self { value: init_value }
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                contract.value = init_value;
+                let caller = Self::env().caller();
+                contract.my_value.insert(&caller, &0);
+            })
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(0)
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                contract.value = Default::default();
+            })
+        }
+
+        #[ink(message)]
+        pub fn get_mine(&self) -> i32 {
+            self.my_value.get(&self.env().caller()).unwrap_or_default()
         }
 
         #[ink(message)]
@@ -37,6 +49,19 @@ mod incrementer {
         #[ink(message)]
         pub fn inc(&mut self, by: i32) {
             self.value += by;
+        }
+
+        #[ink(message)]
+        pub fn inc_mine(&mut self, by: i32) {
+            let caller = self.env().caller();
+            let my_value = self.get_mine();
+
+            self.my_value.insert(caller, &(my_value + by));
+        }
+
+        #[ink(message)]
+        pub fn remove_mine(&mut self) {
+            self.my_value.remove(&self.env().caller())
         }
     }
 
@@ -69,6 +94,32 @@ mod incrementer {
 
             contract.inc(-50);
             assert_eq!(contract.get(), -3);
+        }
+
+        #[ink::test]
+        fn inc_mine_works() {
+            let mut contract = Incrementer::new(11);
+
+            assert_eq!(contract.get_mine(), 0);
+
+            contract.inc_mine(5);
+            assert_eq!(contract.get_mine(), 5);
+
+            contract.inc_mine(5);
+            assert_eq!(contract.get_mine(), 10);
+        }
+
+        #[ink::test]
+        fn remove_mine_works() {
+            let mut contract = Incrementer::new(11);
+
+            assert_eq!(contract.get_mine(), 0);
+
+            contract.inc_mine(5);
+            assert_eq!(contract.get_mine(), 5);
+
+            contract.remove_mine();
+            assert_eq!(contract.get_mine(), 0);
         }
     }
 }
