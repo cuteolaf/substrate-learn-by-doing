eecec02cbede12d1eba3c81c7a31c361ac75d2fd
cuteolaf <oliverlim818@gmail.com>
2022-08-16 21:45:13 -0700

    feat: incrementer contract

---
+++ smart-contract/incrementer/lib.rs
@@ -0,0 +1,74 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod incrementer {
+
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    pub struct Incrementer {
+        /// Stores a single `bool` value on the storage.
+        value: i32,
+    }
+
+    impl Incrementer {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(init_value: i32) -> Self {
+            Self { value: init_value }
+        }
+
+        /// Constructor that initializes the `bool` value to `false`.
+        ///
+        /// Constructors can delegate to other constructors.
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self::new(0)
+        }
+
+        #[ink(message)]
+        pub fn get(&self) -> i32 {
+            self.value
+        }
+
+        #[ink(message)]
+        pub fn inc(&mut self, by: i32) {
+            self.value += by;
+        }
+    }
+
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    /// module and test functions are marked with a `#[test]` attribute.
+    /// The below code is technically just normal Rust code.
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// Imports `ink_lang` so we can use `#[ink::test]`.
+        use ink_lang as ink;
+
+        /// We test if the default constructor does its job.
+        #[ink::test]
+        fn default_works() {
+            let incrementer = Incrementer::default();
+            assert_eq!(incrementer.get(), 0);
+        }
+
+        #[ink::test]
+        fn it_works() {
+            let mut contract = Incrementer::new(42);
+
+            assert_eq!(contract.get(), 42);
+
+            contract.inc(5);
+            assert_eq!(contract.get(), 47);
+
+            contract.inc(-50);
+            assert_eq!(contract.get(), -3);
+        }
+    }
+}
